52ccfb2ab1faba714ddaf99e899f6bdd1d421579
Andriy Lekh <lehandriy@gmail.com>
2020-01-02 15:59:08 +0200

    Small run script improvements

--- run.sh
+++ run.sh
@@ -1,35 +1,35 @@
 echo express >> results.txt ;
 node expressserver.js & sleep 5 ;
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
+ab -k -n 1000 -c 10 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 pkill -f expressserver ;
 sleep 5
 
 echo node >> results.txt ;
 node rawnodeserver.js & sleep 5 ;
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
+ab -k -n 1000 -c 10 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 pkill -f rawnodeserver ;
 sleep 5
 
 echo restify >> results.txt ;
 node restifyserver.js & sleep 5 ;
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
+ab -k -n 1000 -c 10 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 pkill -f restifyserver ;
 sleep 5
 
 echo fastify >> results.txt ;
 node fastifyserver.js & sleep 5 ;
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
+ab -k -n 1000 -c 10 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 pkill -f fastifyserver ;
 sleep 5
 
 echo koa >> results.txt ;
 node koa2server.js & sleep 5 ;
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
+ab -k -n 1000 -c 10 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 pkill -f koa2server ;
 sleep 5
 
 echo total.js >> results.txt ;
 node totalserver/total.js & sleep 5 ;
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
+ab -k -n 1000 -c 10 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 pkill -f total ;
 sleep 5 ;
